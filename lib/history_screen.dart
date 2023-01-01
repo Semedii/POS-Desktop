ea92702e5c3d90bc9eb1ca7b0fb5467883f549d1
semedii <abdisamed2023@hotmail.com>
2023-01-01 21:03:34 +0300

    adds save day button

--- lib/history_screen.dart
+++ lib/history_screen.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pos_app/Data/sale_history_data.dart';
+import 'package:intl/intl.dart';
 
 import 'Models/history.dart';
 
 class HistoryScreen extends StatelessWidget {
-  const HistoryScreen({
+   HistoryScreen({
     super.key});
 
+double _getTotalSalePerDay(DateTime date, List<History> list){
+  double totalSale = 0;
+  String Date = DateFormat('yyyy-MM-dd').format(date);
+  for(int i = 0; i<list.length; i++){
+    if(Date.compareTo(DateFormat('yyyy-MM-dd').format(list[i].date)) == 0){
+     totalSale += list[i].total;
+    }
 
+  }
+  return totalSale;
+ 
+}
+
+  _sendDailyReport(){
+    SaleHistoryData.saleHistoryData.clear();
+   
+  }
   
   @override
   Widget build(BuildContext context) {
+    
     return ListView(
       padding: EdgeInsets.all(20),
       children: [
@@ -24,6 +42,17 @@ class HistoryScreen extends StatelessWidget {
         ),
         SizedBox(height: 20,),
         ...SaleHistoryData.saleHistoryData.map((e) => History(productName: e.productName, total: e.total, date: e.date,)).toList(),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Total sale      ", style: TextStyle(color: Colors.white),),
+            Text(" ${_getTotalSalePerDay(DateTime.now(), SaleHistoryData.saleHistoryData)}",  style: TextStyle(color: Colors.white),)
+          ],
+        ),
+        ElevatedButton(onPressed: (){
+          _sendDailyReport();
+        }, child: Text("Save Day"))
     ]);
     
     
